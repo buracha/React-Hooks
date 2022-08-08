e3639f413278ee2b4291483171b1e7144705fe88
buracha <jadsoncanuto6@gmail.com>
2022-08-08 07:38:44 -0300

    Exemplo 01 ao 07

---
+++ src/hooks/Exemplo07_useContext.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Principal(){
+    return(
+        <h1>Componente Principal </h1>
+    )
+}
+
+export default Principal;
+
+//function Exemplo07_useContext(){
+    
+    //return(
+        //<div>
+
+       //</div>
+    //)
+//}
+
+//export default Exemplo07_useContext
--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Exemplo01_useState from './hooks/Exemplo01_useState';
+import Exemplo02_useState from './hooks/Exemplo02_useState';
+import Exemplo03_useState from './hooks/Exemplo03_useState';
+import Exemplo04_useState from './hooks/Exemplo04_useState';
+import Exemplo05_useEffect from './hooks/Exemplo05_useEffect';
+import Exemplo06_useEffect from './hooks/Exemplo06_useEffect';
+import Exemplo07_useContext from './hooks/Exemplo07_useContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Exemplo07_useContext />
   </React.StrictMode>
 );
 
